c8c682aa3c64eda675f137b71ba97b1b40d8dd40
Busisiwetshongozi <busisiwetshongozi@gmail.com>
2024-04-16 16:35:55 +0200

    calculator

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,17 @@
 class Calculator{
     add(numbers){
-
-    }
+        if(numbers ==""){
+            return 0;
+            
+    
+        }
+        else if (number.length = 1  ){
+            return number
+        }
+    };
+    
 }
+
+
 
 module.exports = Calculator
